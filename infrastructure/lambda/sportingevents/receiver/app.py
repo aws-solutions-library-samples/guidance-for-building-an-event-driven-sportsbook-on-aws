da4ef851a589af8f8639f588286a3a8160ba7f68
Papageorgiou <dpapageo@b0be835d057a.ant.amazon.com>
2024-03-28 14:08:06 +0200

    integrate with existing events table

--- infrastructure/lambda/sportingevents/receiver/app.py
+++ infrastructure/lambda/sportingevents/receiver/app.py
@@ -1,40 +1,75 @@
+from os import getenv
 import boto3
 import json
 
+from crhelper import CfnResource
 from aws_lambda_powertools import Logger
+from datetime import datetime
 
-dynamodb_client = boto3.client('dynamodb')
 logger = Logger()
 
-def lambda_handler(event, context):
-  betting_events = json.loads(event.get('body'))
-  for betting_event in betting_events:
-    try:
-      write_event_to_dynamo(betting_event)
-    except:
-      return {
-        'statusCode': 400,
-        'body': 'Bad Request!'
-      }
-  return {
-    'statusCode': 200,
-    'body': 'Successfully inserted event!'
-  }
+helper = CfnResource(json_logging=False, log_level='DEBUG',
+                     boto_level='CRITICAL')
 
-def write_event_to_dynamo(betting_event):
-  try:
-    event_id = betting_event['eventId']
-    home_team = betting_event['homeTeam']
-    away_team = betting_event['awayTeam']
-    start_time = betting_event['startTime']
-    duration = betting_event['duration']
-    state = betting_event['state']
-    if None  in (event_id, home_team, away_team, start_time, duration, state):
-      raise Exception("Null values are not allowed")
-    logger.info(event_id)
-    dynamodb_client.put_item(TableName='BettingEvents', Item={'id': {'S': event_id}, 'homeTeam': {'S': home_team},
-                                                              'awayTeam': {'S': away_team}, 'startTime': {'N': str(start_time)}, 'duration': {'N': str(duration)}, 'state': {'S': state}})
-    logger.info("Event saved!")
-  except Exception:
-    logger.exception(Exception)
-    raise Exception
+try:
+    table_name = getenv('DB_TABLE')
+    session = boto3.Session()
+    dynamodb = session.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+except Exception as e:
+    helper.init_failure(e)
+
+def lambda_handler(event, context):
+    betting_events = json.loads(event.get('body'))
+    date_format = '%Y-%m-%dT%H:%M:%SZ'
+    for betting_event in betting_events:
+        try:
+            write_event_to_dynamo(betting_event, date_format)
+        except:
+            return {
+                'statusCode': 400,
+                'body': 'Bad Request!'
+            }
+    return {
+        'statusCode': 200,
+        'body': 'Successfully inserted event!'
+    }
+
+
+def write_event_to_dynamo(betting_event, date_format):
+    try:
+        with table.batch_writer() as batch:
+            event_id = betting_event['eventId']
+            home_team = betting_event['homeTeam']
+            away_team = betting_event['awayTeam']
+
+            start_time = datetime.fromtimestamp(betting_event['startTime']/1000.0).strftime(date_format)
+            end_time = datetime.fromtimestamp(betting_event['endTime']/1000.0).strftime(date_format)
+            updated_at = datetime.fromtimestamp(betting_event['updatedAt']/1000.0).strftime(date_format)
+            duration = betting_event['duration']
+            state = betting_event['state']
+            away_odds = betting_event['awayOdds']
+            draw_odds = betting_event['drawOdds']
+            home_odds = betting_event['homeOdds']
+            if None in (event_id, home_team, away_team, start_time, duration, state):
+                raise Exception("Null values are not allowed")
+            logger.info(event_id)
+            item = {
+                'eventId': event_id,
+                'home': home_team,
+                'away': away_team,
+                'start': str(start_time),
+                'updatedAt': str(updated_at),
+                'end': str(end_time),
+                'duration': str(duration),
+                'homeOdds': home_odds,
+                'state': state,
+                'drawOdds': draw_odds,
+                'awayOdds': away_odds
+            }
+            print(item)
+            batch.put_item(Item=item)
+        logger.info("Event saved!")
+    except Exception:
+        logger.exception(Exception)
+        raise Exception
